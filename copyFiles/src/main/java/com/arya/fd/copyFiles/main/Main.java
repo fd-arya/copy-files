3c3b52bc34a13dcfd27b39832840ebe39d330a96
fd-arya <devil002farshid@yahoo.com>
2018-05-09 08:14:36 -0400

    linux

--- copyFiles/src/main/java/com/arya/fd/copyFiles/main/Main.java
+++ copyFiles/src/main/java/com/arya/fd/copyFiles/main/Main.java
@@ -13,28 +13,34 @@ import com.arya.fd.copyFiles.business.logic.RenameFiles;
 import com.arya.fd.copyFiles.business.service.internal.security.Encryption;
 import com.arya.fd.copyFiles.model.GetFileDirectory;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
 	/*
 	 * fd_arya 12/04/2018
 	 */
 
-	public static void main(String[] args) {
-		GetFileDirectory directory = new GetFileDirectory();
-		CopyingFiles copyFile = new CopyingFiles();
-		MovingFiles movingFiles = new MovingFiles();
-		DeleteFiles deleteFiles = new DeleteFiles();
-		RenameFiles renameFiles = new RenameFiles();
-		EncryptFile encryptFile = new EncryptFile();
-		DecryptFile decryptFile = new DecryptFile();
+	public static void main(String[] args)  {
+//		GetFileDirectory directory = new GetFileDirectory();
+//		CopyingFiles copyFile = new CopyingFiles();
+//		MovingFiles movingFiles = new MovingFiles();
+//		DeleteFiles deleteFiles = new DeleteFiles();
+//		RenameFiles renameFiles = new RenameFiles();
+//		EncryptFile encryptFile = new EncryptFile();
+//		DecryptFile decryptFile = new DecryptFile();
+//
+//		directory.setSource("E:\\1.txt");
+//		directory.setDest("C:\\");
+//
+//		File fileSource = new File(directory.getSource());
+//		File fileDestination = new File(directory.getDest() + fileSource.getName());
 
-		directory.setSource("E:\\1.txt");
-		directory.setDest("C:\\");
-
-		File fileSource = new File(directory.getSource());
-		File fileDestination = new File(directory.getDest() + fileSource.getName());
-
-		try {
+//		try {
 			/*----------copy file-----------*/
 			// copyFile.copyFileStream(fileSource, fileDestination);
 
@@ -52,14 +58,27 @@ public class Main {
 			// encryptFile.encrypt(fileSource);
 
 			/*----------dencryption file-----------*/
-			decryptFile.dencrypt(fileSource); // not working...
+//			decryptFile.dencrypt(fileSource); // not working...
 
-		} catch (IOException e) {
-			log(e);
-		}
+//		} catch (IOException e) {
+//			log(e);
+//		}
+	
+	
+	launch(args);
+	
+	
 	}
 
 	private static void log(Object log) {
 		System.out.println(String.valueOf(log));
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/arya/fd/copyFiles/view/pages/MainFrom.fxml"));
+		Scene scene = new Scene(fxmlLoader.<BorderPane>load());
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
---
+++ copyFiles/src/main/java/com/arya/fd/copyFiles/view/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.arya.fd.copyFiles.view.controller;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+	@FXML
+	public void mnuClose() {
+		System.exit(0);
+	}
+}
